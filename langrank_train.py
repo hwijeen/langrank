6dcdd902d937f399677813573c2d0ae4a432692c
hwijeen <hwijeen@gmail.com>
2020-03-15 17:55:17 +0900

    train for sa in prog

--- langrank_train.py
+++ langrank_train.py
@@ -39,5 +39,28 @@ def train_olid():
           feature_name=feature_name, task="OLID")
     assert os.path.isfile(output_model)
 
+def train_sa():
+    langs=["ara", "chi", "dut", "eng", "fre",
+           "ger", "jap", "kor", "per", "rus",
+           "spa", "tam", "tha", "tur"]
+    data_dir = 'datasets/sa/'
+    datasets = [os.path.join(data_dir, f'{l}.txt') for l in langs]
+    rank = [[0, 4, 2, 1, 3],
+            [2, 0, 4, 1, 3],
+            [2, 4, 0, 1, 3],
+            [3, 1, 4, 0, 2],
+            [3, 1, 4, 2, 0]]
+    tmp_dir = "tmp"
+    preprocess = build_preprocess()
+    prepare_train_file(datasets=datasets, langs=langs, rank=rank,
+                       tmp_dir=tmp_dir, task="sa", preprocess=preprocess)
+    output_model = "{}/model_child_1.txt".format(tmp_dir)
+    feature_name = ['word_overlap', 'transfer_data_size', 'task_data_size',
+                    'ratio_data_size', 'transfer_ttr', 'task_ttr', 'distance_ttr',
+                    'genetic', 'syntactic', 'featural', 'phonological', 'inventory', 'geographical']
+    train(tmp_dir=tmp_dir, output_model=output_model,
+          feature_name=feature_name, task="OLID")
+    assert os.path.isfile(output_model)
+
 if __name__ == '__main__':
     train_olid()
