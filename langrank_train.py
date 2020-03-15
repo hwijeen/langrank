f87d057f4321a17aacb80104d7e3e3fba64dcfd5
jiminsun <jiminxsun@gmail.com>
2020-03-15 20:06:26 +0900

    added evaluation ft

--- langrank_train.py
+++ langrank_train.py
@@ -2,8 +2,9 @@ import os, sys
 root=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(root)
 # import pytest
-from langrank import prepare_train_file, train
+from langrank import prepare_train_file, train, rank_to_relevance
 from preprocessing import build_preprocess
+from sklearn.metrics import ndcg_score
 
 def test_train_mt():
     langs = ["aze", "ben", "fin"]
@@ -39,6 +40,16 @@ def train_olid():
     train(tmp_dir=tmp_dir, output_model=output_model,
           feature_name=feature_name, task="OLID")
     assert os.path.isfile(output_model)
+
+
+def evaluate(pred_ranking, gold_ranking):
+    num_lang = len(pred_ranking)
+    pred_rel = rank_to_relevance(pred_ranking, num_lang)
+    gold_rel = rank_to_relevance(gold_ranking, num_lang)
+    pred_topk = [pred_rel[idx] for idx, rel in enumerate(gold_rel) if rel > 0]
+    gold_topk = [rel for idx, rel in enumerate(gold_rel) if rel > 0]
+    return ndcg_score(y_score=pred_topk, y_true=gold_topk)
+
 
 if __name__ == '__main__':
     train_olid()
